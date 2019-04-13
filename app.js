5c5ccf2bdc3a1f4545802f17e8b99f6203988767
Eason <leoling127@gmail.com>
2019-04-13 12:24:49 +0800

    Add detail and search

--- app.js
+++ app.js
@@ -13,19 +13,41 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }), fileUpload(), express.static('public'))
 mongoose.connect('mongodb://127.0.0.1/restaurant', { useNewUrlParser: true })
 
+//首頁
 app.get('/', (req, res) => {
   Restaurant.find((err, restaurants) => {
     res.render('index', { restaurants })
   })
 })
 
+//搜尋依餐廳、分類
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const searchRegex = new RegExp(keyword, 'i')
+  Restaurant.find(
+    {
+      $or: [{ name: { $regex: searchRegex } }, { category: { $regex: searchRegex } }]
+    },
+    (err, restaurants) => {
+      res.render('index', { restaurants, keyword })
+    }
+  )
+})
+
+//詳細資料頁面
+app.get('/detail/:_id', (req, res) => {
+  Restaurant.findById(req.params._id, (err, restaurant) => {
+    if (err) console.error(err)
+    res.render('detail', { restaurant })
+  })
+})
+
+//新增頁面
 app.post('/add', (req, res) => {
   const uploadFile = req.files.uploadFile
   uploadFile.mv(`./public/img/${uploadFile.name}`, function(err) {
     if (err) return res.status(500).send(err)
   })
-
-  console.log(req.body.name)
   res.redirect('/')
 })
 
